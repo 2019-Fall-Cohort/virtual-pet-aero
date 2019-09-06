f5a2dd173bf21ac459dd538b8d2323d11cb14b9e
Richard Wilson <rcwilson88@gmail.com>
2019-09-06 15:08:40 -0400

    Added new functions

--- VirtualPet.js
+++ VirtualPet.js
@@ -1,5 +1,61 @@
 class VirtualPet {
 
+
+    constructor() {
+        this.hunger = 100;
+        this.happiness = 100;
+
+    }
+
+    feedPet(foodAmount) {
+        let mealSize = 0
+                switch(foodAmount.toLowerCase()){
+            case "treat": mealSize = 2;
+            break;
+            
+            case "snack": mealSize = 5;
+            break;
+
+            case "steak": mealSize = 10;
+            break;
+
+            default: 
+            break;
+        }
+       
+       this.hunger = this.hunger - mealSize
+
+       return mealSize
+        // return this.hunger
+    
+    }
+    playWithPet(playType) {
+        let happinessAmount = 0;
+        let exertion = 0;
+        
+            switch(playType.toLowerCase()){
+            case "ball": 
+                happinessAmount = 2;
+                exertion = 2;
+            break;
+            
+            case "frisbee":  
+                happinessAmount = 5;
+                exertion = 5;
+            break;
+
+            case "stick":  
+                happinessAmount = 10;
+                exertion = 10;
+            break;
+
+            default: 
+            break;
+        }
+       
+       this.happiness = this.happiness + happinessAmount;
+       this.hunger = this.hunger + exertion;
+    }
 }
 
-export default VirtualPet
+module.exports = VirtualPet;
--- VirtualPet.test.js
+++ VirtualPet.test.js
@@ -1,19 +1,44 @@
-const TestRunner = require("jest-runner")
-const TestVirtualPet = require("./VirtualPet").default;
+// const TestRunner = require("jest-runner");
+const VirtualPet = require("./VirtualPet");
 
 describe("VirtualPet", () => {
     describe("feedPet", () => {
         test(" ", () => {
             // Arrange
             
-           const TestVirtualPet = new VirtualPet;
+           const TestVirtualPet =  new VirtualPet();
 
             // Act
             
-        
+            const initialHunger = TestVirtualPet.hunger;
+            TestVirtualPet.feedPet("steak");
+
             //Assert
-            
+         //   expect(TestVirtualPet.hunger).toEqual(98);
+            expect(initialHunger - TestVirtualPet.hunger).toEqual(10)
         })
 
     })
-})
+
+
+        describe("playWithPet", () => {
+            test(" ", () => {
+                // Arrange
+                
+               const TestVirtualPet =  new VirtualPet();
+    
+               
+                // Act
+
+                const initialHunger = TestVirtualPet.hunger;
+                const initialHappiness = TestVirtualPet.happiness;
+                TestVirtualPet.playWithPet("frisbee");
+    
+                //Assert
+             
+                expect(TestVirtualPet.happiness - initialHappiness).toEqual(5);
+                expect(TestVirtualPet.hunger - initialHunger).toEqual(5);
+            })
+    
+        })
+    })
