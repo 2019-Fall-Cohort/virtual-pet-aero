533be4e9c6a3fa79ff2eb9ccc9af7e6f09d68568
Aden802 <ab.aden33@gmail.com>
2019-09-17 13:57:35 -0400

    I created OrganicVirtualPet.js

---
+++ OrganicVirtualPet.js
@@ -0,0 +1,15 @@
+class OrganicVirtualPet{
+
+
+
+    constructor(hunger, thirst, exertion){
+
+        this.hunger = hunger;
+        this.thirst = thirst;
+        this.exertion = exertion;
+
+    }
+
+
+}
+module.exports = OrganicVirtualPet
